7f0e7e9d5e3e8fb8c3473e049e539ddda32b8503
MichaelJahns <jahnsmic001@gmail.com>
2021-03-25 09:01:56 -0700

    Trying to think through how NavigationFragment can influence its parent MainActivity w/o the use of a ViewModel. I dont really know the standard way to handle fragmenttransactions in java

--- app/src/main/java/com/leyline/salmoncookies/views/NavigationFragment.java
+++ app/src/main/java/com/leyline/salmoncookies/views/NavigationFragment.java
@@ -2,15 +2,20 @@ package com.leyline.salmoncookies.views;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.leyline.salmoncookies.R;
 
 public class NavigationFragment extends Fragment {
+    private BottomNavigationView bottomNavigationView;
     public NavigationFragment() {
         // Required empty public constructor
     }
@@ -18,7 +23,19 @@ public class NavigationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_navigation, container, false);
+        View view = inflater.inflate(R.layout.fragment_navigation, container, false);
+        bottomNavigationView = view.findViewById(R.id.bottomNavigationView);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
     }
 }
